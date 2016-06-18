87e1a4a57cf44c05a752c81ed7931f0b2ff158fc
m00re <jens.mittag@gmail.com>
2016-06-18 15:33:57 +0200

    Neueste Version

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'HTML5 / Responsive Core Template',
+    'description' => 'A basic HTML5-based website template to kick-off responsive webpages',
+    'category' => 'template',
+    'author' => 'Jens Mittag',
+    'author_company' => '',
+    'author_email' => 'kontakt@jensmittag.de',
+    'dependencies' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '0.1.0',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.5.0-0.0.0',
+            'typo3' => '7.0.0-0.0.0'
+        )
+    )
+);
+
+?>
